91ef972b3e73d90b9121835014bd8e3055030d5d
olive <olivine.dogue@gmail.com>
2014-07-13 10:48:24 -0400

    scala ftw

--- profundus/src/in/dogue/profundus/Profundus.scala
+++ profundus/src/in/dogue/profundus/Profundus.scala
@@ -1,6 +1,7 @@
 package in.dogue.profundus
 
 import in.dogue.antiqua.graphics.TileRenderer
+import in.dogue.profundus.graphics.AugTileRenderer
 
 object Profundus {
   implicit def tileRenderer2Aug(tr:TileRenderer) = new AugTileRenderer(tr)
