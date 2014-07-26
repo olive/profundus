3ff86ee775b930bf806019a471f384e0c7dbf764
olive <olivine.dogue@gmail.com>
2014-07-26 14:13:41 -0400

    remove existential from standard entity

--- profundus/src/in/dogue/profundus/entities/StandardEntity.scala
+++ profundus/src/in/dogue/profundus/entities/StandardEntity.scala
@@ -7,7 +7,6 @@ import in.dogue.antiqua.data.Direction
 import in.dogue.profundus.particles.{DeathParticle, Particle}
 import in.dogue.antiqua.graphics.TileRenderer
 import in.dogue.profundus.lighting.LightSource
-import in.dogue.profundus.audio.SoundManager
 
 object StandardEntity {
   def create[T](up:T => (Int, Int, Cell, TerrainCache, PlayerInfo, Random) => (T, Cell, Seq[GlobalMessage]),
@@ -23,21 +22,51 @@ object StandardEntity {
     StandardEntity[T](up, dr, onMove, self, light, Alive, canFly, selfType, health, startT)
   }
 
+  def apply[A](aup:A => (Int, Int, Cell, TerrainCache, PlayerInfo, Random) => (A, Cell, Seq[GlobalMessage]),
+               adr:A => Cell => TileRenderer => TileRenderer,
+               aonMove:A => (Direction => Option[WorldTile]) => A,
+               aself:A,
+               alight:LightSource,
+               alive:LivingState,
+               acanFly:Boolean,
+               aselfType:Option[DamageType],
+               ahealth:Int,
+               at:Int) = new StandardEntity {
+    override type T = A
+    override val up = aup
+    override val dr = adr
+    override val onMove = aonMove
+    override val self = aself
+    override val light = alight
+    override val live = alive
+    override val canFly = acanFly
+    override val selfType = aselfType
+    override val health = ahealth
+    override val t = at
+  }
+
   def NoMove[T](t:T)(f:Direction => Option[WorldTile]):T = t
 }
 
 
-case class StandardEntity[T] private (up:T => (Int, Int, Cell, TerrainCache, PlayerInfo, Random) => (T, Cell, Seq[GlobalMessage]),
-                                      dr:T => Cell => TileRenderer => TileRenderer,
-                                      onMove:T => (Direction => Option[WorldTile]) => T,
-                                      self:T,
-                                      light:LightSource,
-                                      live:LivingState,
-                                      canFly:Boolean,
-                                      selfType:Option[DamageType],
-                                      health:Int,
-                                      t:Int) {
-  def update(pos:Cell, cache:TerrainCache, pi:PlayerInfo, r:Random):(StandardEntity[T], Cell, Seq[GlobalMessage]) = {
+trait StandardEntity {
+  type T
+  val up:T => (Int, Int, Cell, TerrainCache, PlayerInfo, Random) => (T, Cell, Seq[GlobalMessage])
+  val dr:T => Cell => TileRenderer => TileRenderer
+  val onMove:T => (Direction => Option[WorldTile]) => T
+  val self:T
+  val light:LightSource
+  val live:LivingState
+  val canFly:Boolean
+  val selfType:Option[DamageType]
+  val health:Int
+  val t:Int
+
+  def copy(self:T=self, live:LivingState=live, health:Int=health, t:Int=t) = {
+    StandardEntity(up, dr, onMove, self, light, live, canFly, selfType, health, t)
+  }
+
+  def update(pos:Cell, cache:TerrainCache, pi:PlayerInfo, r:Random):(StandardEntity, Cell, Seq[GlobalMessage]) = {
     if (pi.live == Dead) {
       return (copy(t=t+1), pos, Seq())
     }
@@ -77,6 +106,6 @@ case class StandardEntity[T] private (up:T => (Int, Int, Cell, TerrainCache, Pla
   def getLight(ij:Cell) = Seq(light.copy(pos=ij))
 
   def toEntity(ij:Cell):Entity = {
-    Entity.create[StandardEntity[T]](ij, canFly.select(Grounded,Floating), _.update, _.move, _.damage, _.kill, _.getDeathParticle, _.getLight, _.getLive, _.draw, this)
+    Entity.create[StandardEntity](ij, canFly.select(Grounded,Floating), _.update, _.move, _.damage, _.kill, _.getDeathParticle, _.getLight, _.getLive, _.draw, this)
   }
 }
